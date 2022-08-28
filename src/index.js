7e04e55a0ff4b3b1c0fd400dd351c1d2e4e77c4a
Pedro Oliveira <pedrodepaivasoaresll@gmail.com>
2022-08-28 20:28:37 -0300

    feat: add a minimal webpack config

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import ReactDOM from 'react-dom/client'
+
+const root = ReactDOM.createRoot(document.querySelector('#root'))
+root.render(<>Hello World</>)
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: process.env.NODE_ENV || "development",
+  entry: path.resolve(__dirname, "src", "index.js"),
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "build"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  devServer: {
+    port: process.env.PORT || 3000,
+    client: {
+      overlay: true,
+      logging: "info",
+    },
+  },
+};
