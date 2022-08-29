f004f5df5d41a8d807c0e22297d57f5754d6fdf4
Pedro Oliveira <pedrodepaivasoaresll@gmail.com>
2022-08-29 15:22:56 -0300

    build: tailwind + css loaders webpack config

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { Test } from './features/Test'
+import './global.css'
 
 export const App = () => {
   return (
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,17 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  mode: process.env.NODE_ENV || "development",
-  entry: path.resolve(__dirname, "src", "index.js"),
+  mode: process.env.NODE_ENV || 'development',
+  entry: path.resolve(__dirname, 'src', 'index.js'),
   output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "build"),
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'build'),
     clean: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "public", "index.html"),
+      template: path.resolve(__dirname, 'public', 'index.html'),
     }),
   ],
   module: {
@@ -20,8 +20,12 @@ module.exports = {
         test: /\.m?js$/,
         exclude: /(node_modules)/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
         },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
       },
     ],
   },
@@ -29,7 +33,7 @@ module.exports = {
     port: process.env.PORT || 3000,
     client: {
       overlay: true,
-      logging: "info",
+      logging: 'info',
     },
   },
-};
+}
