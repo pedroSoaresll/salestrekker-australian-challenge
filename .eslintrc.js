a4a432fe3461afee72f062bc0479fd4d5d14b525
Pedro Oliveira <pedrodepaivasoaresll@gmail.com>
2022-08-30 17:47:25 -0300

    chore: install and config jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,18 +2,20 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    'jest/globals': true,
   },
   extends: [
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
     'plugin:react/jsx-runtime',
+    'plugin:jest/recommended',
     'prettier',
   ],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['react', 'jsx-a11y', 'prettier'],
+  plugins: ['react', 'jsx-a11y', 'prettier', 'jest'],
   rules: {
     'prettier/prettier': 'error',
     'no-unused-vars': 'error',
