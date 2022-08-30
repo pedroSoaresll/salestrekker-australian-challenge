025ebe03467c2111f31a18c9638fb13b6e54863a
Pedro Oliveira <pedrodepaivasoaresll@gmail.com>
2022-08-30 15:41:30 -0300

    feat: remove border

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -4,7 +4,7 @@ export const Card = ({ children, className, ...props }) => {
   return (
     <div
       {...props}
-      className={`rounded-md border border-violet-100 shadow-md w-3/4 ${className}`}
+      className={`rounded-md border-violet-100 shadow-md w-3/4 ${className}`}
     >
       {children}
     </div>
